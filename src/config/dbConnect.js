1eed1b6b804da8513ace94158467b97949121d38
Lucas Lourenço Miranda <lucas.adereco@gmail.com>
2023-03-29 15:50:53 -0300

    cira variaveis de ambiente

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect(
-  "mongodb+srv://lucaslmir:GVIf1IOhVoA2a8J9@cluster0.4typgyo.mongodb.net/alura-node"
-);
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 
 let db = mongoose.connection
 
